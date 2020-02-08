a26b0fa437bfef1b16e92cb7fd40cf96d36829f5
Inara <vafamustafayeva4@gmail.com>
2020-02-08 11:01:26 +0400

    -- 1

--- src/main/java/AAA.java
+++ src/main/java/AAA.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class AAA {
     public static void main(String[] args) {
-        System.out.println(1);
+        System.out.println("Hello what is your name?");
+        Scanner in= new Scanner(System.in);
+        String name= in.next();
+        System.out.println("Hello "+ name);
+        System.out.println("Nice to meet you "+ name);
+        System.out.println("Bye "+ name);
     }
 }
