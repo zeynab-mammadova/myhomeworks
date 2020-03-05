bd4447d96fd713ea41368f9ff0ca4348ca9d8718
zeynab-mammadova <zeynebb2022@gmail.com>
2020-03-05 22:18:21 +0400

    commited

---
+++ src/main/java/az/company/project/booking/entity/RandomFlights.java
@@ -0,0 +1,11 @@
+package az.company.project.booking.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomFlights {
+    public List<Flight> create(){
+        ArrayList<Flight> flights =new ArrayList<>();
+
+    }
+}
