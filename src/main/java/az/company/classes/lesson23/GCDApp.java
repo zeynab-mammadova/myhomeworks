72f3270ac423ad3980fefdda89a0232894c5fda9
zeynab-mammadova <zeynebb2022@gmail.com>
2020-03-24 09:27:29 +0400

    first

---
+++ src/main/java/az/company/classes/lesson23/GCDApp.java
@@ -0,0 +1,18 @@
+package az.company.classes.lesson23;
+
+public class GCDApp {
+  public static int gcd(int a, int b) {
+    if (b == 0)
+      return a;
+    return gcd(b, a % b);
+  }
+
+
+  public static void main(String[] args) {
+    System.out.println(gcd(7,5));
+    System.out.println(gcd(20,5));
+    System.out.println(gcd(36,24));
+    System.out.println(gcd(3,9));
+
+  }
+}
